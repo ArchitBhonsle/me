8527cb722e63d9d03efd3e2b007b6837dbb077e5
Archit Bhonsle <abhonsle2000@gmail.com>
2023-11-06 01:04:40 -0500

    site and base in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://architbhonsle.github.io',
+    base: '/me',
     integrations: [tailwind()],
 });
